ede77b3e8f40f9b3f6f1bc24de474e4b4a7cc870
kanishk <kanishk@kanishks-MacBook-Air.local>
2026-07-11 13:08:57 +0530

    fix: build issue

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,3 +1,10 @@
-export { runtime, size, contentType, default } from "./opengraph-image";
+import { renderOgImage } from "@/lib/og-image";
 
+export const runtime = "edge";
 export const alt = "Squadr, home of Sports Squad Forge";
+export const size = { width: 1200, height: 630 };
+export const contentType = "image/png";
+
+export default function TwitterImage() {
+  return renderOgImage();
+}
